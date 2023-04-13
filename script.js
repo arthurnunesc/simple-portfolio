270e7d9c4b511882ac545ee8f12792477216c313
Arthur Nunes <arthurnunesc@proton.me>
2023-04-13 13:06:37 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Mukta", "sans-serif"],
+      },
+      colors: {
+        DarkerCyan: "hsl(179, 62%, 41%)",
+        Cyan: "hsl(179, 62%, 43%)",
+        BrightYellow: "hsl(71, 73%, 54%)",
+        LightGray: "hsl(204, 43%, 93%)",
+        GrayishBlue: "hsl(218, 22%, 67%)",
+      },
+    },
+  },
+};
